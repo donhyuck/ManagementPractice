c8fa782bb7d2935e6a4589f70cc46721296e301a
donhyuck <leedonhyuck123@gmail.com>
2022-04-06 18:17:06 +0900

    디스패쳐 서블릿과 컨트롤러 구조 도입중03 / 게시글 리스트 개선

--- src/main/java/com/ldh/java/mp/controller/ArticleController.java
+++ src/main/java/com/ldh/java/mp/controller/ArticleController.java
@@ -1,9 +1,16 @@
 package com.ldh.java.mp.controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ldh.java.mp.util.DBUtil;
+import com.ldh.java.mp.util.SecSql;
 
 public class ArticleController {
 
@@ -15,6 +22,46 @@ public class ArticleController {
 		this.request = request;
 		this.response = response;
 		this.conn = conn;
+	}
+
+	public void actionList() throws ServletException, IOException {
+
+		// 페이지 처음
+		int page = 1;
+
+		if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
+
+			try {
+				page = Integer.parseInt(request.getParameter("page"));
+			} catch (NumberFormatException e) {
+			}
+		}
+
+		// 페이지 구분
+		int itemsInAPage = 15;
+		int limitFrom = (page - 1) * itemsInAPage;
+
+		// 페이지에 따라 페이지 갯수 조절
+		SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
+		sql.append("FROM article");
+
+		int totalCount = DBUtil.selectRowIntValue(conn, sql);
+		int totalpage = (int) Math.ceil((double) totalCount / itemsInAPage);
+
+		// 게시글 전체 목록 보기
+		sql = SecSql.from("SELECT *");
+		sql.append("FROM article");
+		sql.append("ORDER BY id DESC");
+		sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
+
+		System.out.println(sql);
+
+		List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+		request.setAttribute("articleRows", articleRows);
+		request.setAttribute("page", page); // 현재 페이지를 알기위해 넘겨준다.
+		request.setAttribute("totalpage", totalpage);
+
+		request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 	}
 
 }
--- src/main/java/com/ldh/java/mp/servlet/ArticleListServlet.java
+++ src/main/java/com/ldh/java/mp/servlet/ArticleListServlet.java
@@ -4,20 +4,15 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.ldh.java.mp.Config;
 import com.ldh.java.mp.exception.SQLErrorException;
-import com.ldh.java.mp.util.DBUtil;
-import com.ldh.java.mp.util.SecSql;
 
 @WebServlet("/article/list")
 public class ArticleListServlet extends HttpServlet {
@@ -46,64 +41,6 @@ public class ArticleListServlet extends HttpServlet {
 
 		try {
 			conn = DriverManager.getConnection(Config.getDBUrl(), Config.getDBId(), Config.getDBPw());
-
-			// 로그인 상태 확인
-			HttpSession session = request.getSession();
-
-			boolean isLogined = false;
-			int loginedMemberId = -1;
-			Map<String, Object> loginedMemberRow = null;
-
-			if (session.getAttribute("loginedMemberId") != null) {
-				loginedMemberId = (int) session.getAttribute("loginedMemberId");
-				isLogined = true;
-
-				SecSql sql = SecSql.from("SELECT * FROM `member`");
-				sql.append("WHERE id = ?", loginedMemberId);
-
-				loginedMemberRow = DBUtil.selectRow(conn, sql);
-			}
-
-			request.setAttribute("isLogined", isLogined);
-			request.setAttribute("loginedMemberId", loginedMemberId);
-			request.setAttribute("loginedMemberRow", loginedMemberRow);
-
-			// 페이지 처음
-			int page = 1;
-
-			if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
-
-				try {
-					page = Integer.parseInt(request.getParameter("page"));
-				} catch (NumberFormatException e) {
-				}
-			}
-
-			// 페이지 구분
-			int itemsInAPage = 15;
-			int limitFrom = (page - 1) * itemsInAPage;
-
-			// 페이지에 따라 페이지 갯수 조절
-			SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
-			sql.append("FROM article");
-
-			int totalCount = DBUtil.selectRowIntValue(conn, sql);
-			int totalpage = (int) Math.ceil((double) totalCount / itemsInAPage);
-
-			// 게시글 전체 목록 보기
-			sql = SecSql.from("SELECT *");
-			sql.append("FROM article");
-			sql.append("ORDER BY id DESC");
-			sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
-
-			System.out.println(sql);
-
-			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
-			request.setAttribute("articleRows", articleRows);
-			request.setAttribute("page", page); // 현재 페이지를 알기위해 넘겨준다.
-			request.setAttribute("totalpage", totalpage);
-
-			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 
 		} catch (SQLException e) {
 			e.printStackTrace();
